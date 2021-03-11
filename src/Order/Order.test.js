c0848d8e8ff68aac39236b3c82ed3835dd632079
Myshonkov Maxim <maxim.myshonkov@gmail.com>
2021-03-11 20:10:03 +0300

    Add sortOrder and Order tests

--- src/Order/Order.test.js
+++ src/Order/Order.test.js
@@ -1,8 +1,61 @@
+/* eslint-disable import/first */
+jest.mock('../utils/getDate');
+
 import React from 'react'
+import {shallow, configure} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import toJson from 'enzyme-to-json';
+import {getDate} from '../utils/getDate';
+import Order from "./Order";
+import {fakeOrders} from "../data/fakeOrders";
+
+configure({adapter: new Adapter()});
+
+getDate.mockReturnValue("11 марта, чт, 2021 год");
 
 describe('Order.js', () => {
-  it('some test', () => {
+  beforeEach(() => {
+    jest.resetModules();
+  });
+  afterAll(() => {
+    jest.resetModules();
+  });
 
+  it('render no order is null', () => {
+    const wrapper = shallow(<Order />);
+    expect(wrapper.getElement()).toBeNull();
+  });
+
+  it('render empty order is null', () => {
+    const wrapper = shallow(<Order order={{}}/>);
+    expect(wrapper.getElement()).toBeNull();
+  });
+
+  it('render order without shop is null', () => {
+    let order = Object.assign({}, fakeOrders[0]);
+    delete order.shop;
+    const wrapper = shallow(<Order order={order}/>);
+    expect(wrapper.getElement()).toBeNull();
+  });
+
+  it('render order without date is null', () => {
+    let order = Object.assign({}, fakeOrders[0]);
+    delete order.date;
+    const wrapper = shallow(<Order order={order}/>);
+    expect(wrapper.getElement()).toBeNull();
+  });
+
+  it('render order without items snapshot', () => {
+    let order = Object.assign({}, fakeOrders[0]);
+    delete order.items;
+    const wrapper = shallow(<Order order={order}/>);
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  it('render fake order snapshot', () => {
+    const order = fakeOrders[0]
+    const wrapper = shallow(<Order order={order}/>);
+    expect(toJson(wrapper)).toMatchSnapshot();
   });
 });
 
--- src/utils/sortOrders.test.js
+++ src/utils/sortOrders.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {sortByItemCount} from './sortOrders';
+import {sortOrders, getSortFunction, sortTypes, sortByItemCount, sortByDate} from './sortOrders';
 
 describe('sortByItemCount function', () => {
 	it('orders are null', () => {
@@ -20,5 +20,90 @@ describe('sortByItemCount function', () => {
 
 		expect(result).toBe(0);
 	});
+
+	it('items are null', () => {
+		const order1 = {
+			items: null,
+		};
+		const order2 = {
+			items: null,
+		};
+		const result = sortByItemCount(order1, order2);
+		expect(result).toBe(0);
+	});
+
+	it('items are not objects', () => {
+		const result = sortByItemCount(undefined, undefined);
+		expect(result).toEqual(0);
+	});
+
+	test.each([
+		[{items: ['item1', 'item2']}, {items: ['1', '2', '3']}, -1],
+		[{items: ['item1', 'item2', 'item3']}, {items: ['1', '2']}, 1],
+	])('sortByItemCount different items count test', (order1, order2, expected) => {
+		const result = sortByItemCount(order1, order2);
+		expect(result).toBe(expected);
+	});
 });
+
+describe('sortByDate function', () => {
+	test.each([
+		[{date: 1552585550000}, {date: 1552585550000}, 0],
+		[{date: 1552585550001}, {date: 1552585550000}, -1],
+		[{date: 1552585550000}, {date: 1552585550001}, 1],
+	])('sortByDate valid dates test', (order1, order2, expected) => {
+		const result = sortByDate(order1, order2);
+		expect(result).toBe(expected);
+	})
+	test.each([
+		[{date: 1552585550000}, {date: null}],
+		[{date: null}, {date: 1552585550000}],
+		[{date: null}, {date: null}],
+		[{date: 1552585550000}, null],
+		[null, {date: 1552585550000}],
+		[null, null],
+		[undefined, undefined],
+	])('sortByDate valid invalid objects test', (order1, order2) => {
+		const result = sortByDate(order1, order2);
+		expect(result).toBe(0);
+	})
+});
+
+describe('sortOrders function', () => {
+	it('sortOrders sort is called', function () {
+		const orders = [1, 2, 3];
+		const sortFunction = jest.fn();
+		sortOrders(orders, sortFunction);
+		expect(sortFunction).toBeCalled();
+	});
+
+	it('sortOrders orders are null', function () {
+		const sortFunction = jest.fn();
+		sortOrders(null, sortFunction);
+		expect(sortFunction).not.toBeCalled();
+	});
+
+	it('sortOrders sortFunction is null', function () {
+		const orders = [2, 3, 1];
+		sortOrders(orders, null);
+		expect(orders).toEqual([2, 3, 1]);
+	});
+
+	it('sortOrders sort results are applied', function () {
+		const sortFunction = jest.fn((l_value, r_value) => l_value - r_value);
+		const orders = [2, 3, 1, 5, 4];
+		sortOrders(orders, sortFunction);
+		expect(orders).toEqual([1, 2, 3, 4, 5]);
+	});
+});
+
+describe('getSortFunction function', () => {
+	test.each([
+		[sortTypes.COUNT, sortByItemCount],
+		[sortTypes.DATE, sortByDate],
+	])('getSortFunction test switch', (sortType, expected) => {
+		expect(getSortFunction(sortType)).toBe(expected);
+	});
+});
+
 
